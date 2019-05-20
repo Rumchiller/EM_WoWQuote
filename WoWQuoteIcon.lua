5c607ac6686c3f8b29e7771f6df3599c5333e9e6
Reginald Leonczuk <leonczuk@outlook.de>
2019-05-20 20:53:12 +0200

    wowquote icon update

--- WoWQuoteIcon.lua
+++ WoWQuoteIcon.lua
@@ -1,5 +1,28 @@
 local db;
 local icon;
+local saveVar;
+
+local iconName			= "WoWQuote";
+local iconImg			= "Interface\\Icons\\inv_misc_book_08";
+local iconOnClick		= function () WQ_ShowUI(); end;
+local iconOnTooltipShow	= function (tooltip) tooltip:SetText(iconName); end;
+
+db = LibStub("LibDataBroker-1.1"):NewDataObject(iconName,
+{
+	type = "data source",
+	text = iconName,
+	icon = iconImg,
+	OnClick = function() iconOnClick(); end,
+	OnTooltipShow = function(tooltip) iconOnTooltipShow(tooltip); end,
+});
+icon = LibStub("LibDBIcon-1.0");
+icon:Register(iconName, db, saveVar);
+
+
+
+
+local db;
+local icon;
 
 db = LibStub("LibDataBroker-1.1"):NewDataObject("WoWQuote",
 {
