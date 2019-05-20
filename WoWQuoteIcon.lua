8529606a5be5926bec4630ec63d48b9864fb01c8
Reginald Leonczuk <leonczuk@outlook.de>
2019-05-20 21:20:33 +0200

    icon bugfix

--- WoWQuoteIcon.lua
+++ WoWQuoteIcon.lua
@@ -17,20 +17,3 @@ db = LibStub("LibDataBroker-1.1"):NewDataObject(iconName,
 });
 icon = LibStub("LibDBIcon-1.0");
 icon:Register(iconName, db, saveVar);
-
-
-
-
-local db;
-local icon;
-
-db = LibStub("LibDataBroker-1.1"):NewDataObject("WoWQuote",
-{
-	type = "data source",
-	text = "WoWQuote",
-	icon = "Interface\\Icons\\inv_misc_book_08",
-	OnClick = function() WQ_ShowUI(); end,
-});
-icon = LibStub("LibDBIcon-1.0");
-
-icon:Register("WoWQuote", db, WoWQuote_savedVarTable);
